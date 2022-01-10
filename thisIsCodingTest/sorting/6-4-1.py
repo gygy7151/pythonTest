b3ee3f73ac9200f0e4890a11c48f76fbea74d6aa
gygy7151 <gygy7151@gmail.com>
2022-01-11 00:23:52 +0900

    feat: 호어정렬 스스로짜기

--- thisIsCodingTest/sorting/6-4-1.py
+++ thisIsCodingTest/sorting/6-4-1.py
@@ -6,4 +6,21 @@ array  = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
 def quick_sort(array, start, end):
     if start >= end :
         return
+
+    pivot = start
+    left = start + 1
+    right = end
+
+    while left < right :
+
+        if array[start] < array[left] and array[start] > array[right] :
+            array[left],  array[right] = array[right], array[left]
+        
+
+        if left >= right :
+
+            if left == right :
+
+            if left > right:
+
     
