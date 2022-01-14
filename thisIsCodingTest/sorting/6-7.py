ff161a4548272ec9da16e63ea132da7422e617d5
gygy7151 <gygy7151@gmail.com>
2022-01-14 23:07:21 +0900

    feat: sort(), sorted() method

---
+++ thisIsCodingTest/sorting/6-7.py
@@ -0,0 +1,14 @@
+'''
+파이썬 정렬 라이브러리
+'''
+# 리스트 변수정렬
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+result = sorted(array)
+print(result)
+
+# 직접 내부원소 정렬 메소드 sort()
+array = [7, 5, 9, 0, 3, 1, 6, 2, 4, 8]
+
+array.sort()
+print(array)
