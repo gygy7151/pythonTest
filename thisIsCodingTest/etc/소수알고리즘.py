8ad2c6f9e3e1659b3750be09b047acdbbecdb2e1
gygy7151 <gygy7151@gmail.com>
2022-01-17 23:25:42 +0900

    feat: 소수의 판별

---
+++ thisIsCodingTest/etc/소수알고리즘.py
@@ -0,0 +1,15 @@
+'''
+소수 판별 함수
+'''
+def is_prime_number(x) :
+
+    for i in range(2, x) :
+
+        if x % i == 0 :
+
+            return False 
+        
+    return True
+
+print(is_prime_number(4))
+print(is_prime_number(7))
