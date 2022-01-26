3648ec037b5ff48e92d60520a97ee04f36c03167
gygy7151 <gygy7151@gmail.com>
2022-01-26 15:13:21 +0900

    feat : 1로만들기 dp활용풀이

--- thisIsCodingTest/dp/8-5.py
+++ thisIsCodingTest/dp/8-5.py
@@ -3,4 +3,27 @@
 연산 사용하는 횟수의 최솟값은?
 '''
 
+        
+x = int(input())
 
+d = [0] * 30001
+
+for i in range(2, x+1) :
+    
+    print(d)
+    d[i] = d[i - 1] + 1
+
+    if i % 2 == 0 :
+
+        d[i] = min(d[i], d[i//2] + 1)
+
+    if i % 3 == 0 :
+
+        d[i] = min(d[i], d[i//3] + 1)
+    
+    if i % 5 == 0 :
+
+        d[i] = min(d[i], d[i//5] + 1)
+
+
+print(d[x])
