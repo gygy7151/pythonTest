0025e58cb86049bfa0a36ba5581b670b6d98e193
gygy7151 <gygy7151@gmail.com>
2022-01-26 23:14:37 +0900

    feat: 개미전사 dp 구현완료

--- thisIsCodingTest/dp/8-6.py
+++ thisIsCodingTest/dp/8-6.py
@@ -1,5 +1,23 @@
 '''
-1로 만들기
-연산 사용하는 횟수의 최솟값은?
-'''
+개미전사
 
+문제가 뭐야?
+
+'''
+n =  int(input())
+stores = list(map(int, input().split()))
+
+for i in range(3, n, 2) :
+
+    stores[i] += stores[i-2]
+
+for i in range(2, n, 2) :
+
+    stores[i] += stores[i-2]
+
+if stores[n-1] > stores[n-2] :
+
+    print(stores[n-1])
+
+else :
+    print(stores[n-2])
