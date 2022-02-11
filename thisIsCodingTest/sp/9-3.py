554762c1cadd900481941245acbe05553acb64c2
gygy7151 <gygy7151@gmail.com>
2022-02-11 09:14:41 +0900

    feat: solved prob 정확한순위

--- thisIsCodingTest/sp/9-3.py
+++ thisIsCodingTest/sp/9-3.py
@@ -18,15 +18,12 @@ for i in range(1, n+1):
 for _ in range(m):
     a, b, c = map(int, input().split())
     graph[a][b] = c
+    graph[b][a] = c
 
 #점화식에 따라 플로이드 워셜 알고리즘을 수행
 for i in range(1, n+1):
     for j in range(1, n+1):
         for k in range(1, n+1):
-            #if i == j :
-            #    continue
-            #if j == k or i == k:
-            #    continue
             graph[j][k] = min(graph[j][k], graph[j][i]+graph[i][k])
 
 #수행된 결과를 출력
