2847d8e842aa4786933b265741968acc2528b62c
gygy7151 <gygy7151@gmail.com>
2022-03-08 21:57:18 +0900

    feat: solved a prob of 1278 of BOJ

--- BOJ/bfs&dfs/2178.py
+++ BOJ/bfs&dfs/2178.py
@@ -1,36 +1,27 @@
-n, m, v = map(int,input().split())
-graph = [[0]* (n+1) for _ in range(n+1)]
-for _ in range(m):
-    i, j = map(int, input().split())
-    graph[i][j] = 1
-    graph[j][i] = 1
-
-d_visited = []
-b_visited = [] 
-def dfs(v):
-    if v in d_visited:
-        return 
-    d_visited.append(v)
-    #아 어차피 graph는 0번째 인덱스요소부터 열이 시작되어 상관없음
-    for idx, i in enumerate(graph[v]):
-        if i == 1 and idx not in d_visited:
-            dfs(idx)
-    return
-
-def bfs(v):
-    q = [v]
-    b_visited.append(v)
+n, m = map(int, input().split())
+graph = []
+for _ in range(n):
+    str = input()
+    graph.append([x for x in str])
+dx = [-1, 1, 0, 0]
+dy = [0, 0, -1, 1]
+def bfs(start):
+    q = []
+    q.append(start)
+    i, j, cnt = start
+    visited = []
+    visited.append((i,j))
     while q:
-        vrtex = q.pop(0)
-        for jdx, j in enumerate(graph[vrtex]):
-            if j == 1 and jdx not in b_visited:
-                q.append(jdx)
-                b_visited.append(jdx)
-    
-dfs(v)
-bfs(v)
-for d_res in d_visited:
-        print(d_res, end =' ')
-print()
-for b_res in b_visited:
-        print(b_res, end=' ')
+        r, k, cnt = q.pop(0)
+        if r == n-1 and k == m-1:
+            return cnt
+
+        for dir in range(4):
+            nx = r + dx[dir]
+            ny = k + dy[dir]
+            if 0 <= nx < n and 0 <= ny < m:
+                if graph[nx][ny] == '1' and (nx, ny) not in visited:
+                    q.append((nx,ny,cnt+1))
+                    visited.append((nx,ny))
+
+print(bfs((0,0,1)))
