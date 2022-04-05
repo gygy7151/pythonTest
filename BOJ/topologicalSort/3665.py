f7a942527c630b2bf8c8e0bbcbe5fdf54c8698ea
gygy7151 <gygy7151@gmail.com>
2022-04-05 23:58:21 +0900

    feat: re-solved 3665 of BOJ

--- BOJ/topologicalSort/3665.py
+++ BOJ/topologicalSort/3665.py
@@ -1,71 +1,53 @@
 '''
 최종순위 - 3665번
 '''
-from collections import deque
-import sys
-
-t = int(sys.stdin.readline())
-
-for i in range(t):
-    n = int(sys.stdin.readline())
-
-    graph = [[] for _ in range(n + 1)]
-    inDegree = [0 for _ in range(n + 1)]
-    queue = deque()
-    answer = []
-    flag = 0
-
-    team = list(map(int, sys.stdin.readline().rstrip().split()))
-
-    for j in range(n - 1):
-        for k in range(j + 1, n):
-            graph[team[j]].append(team[k])
-            inDegree[team[k]] += 1
-
-    m = int(sys.stdin.readline())
-    for j in range(m):
-        first, second = map(int, sys.stdin.readline().rstrip().split())
-        flag = True
-
-        for k in graph[first]:
-            if k == second:
-                graph[first].remove(second)
-                inDegree[second] -= 1
-                graph[second].append(first)
-                inDegree[first] += 1
-                flag = False
-
-        if flag:
-            graph[second].remove(first)
-            inDegree[first] -= 1
-            graph[first].append(second)
-            inDegree[second] += 1
-
-    for j in range(1, n + 1):
-        if inDegree[j] == 0:
-            queue.append(j)
-
-    if not queue:
-        print("IMPOSSIBLE")
+for _ in range(int(input())):
+    n = int(input())
+    rank = list(map(int, input().split()))
+    indegree = [-1] + [0] * n
+    temp = [[] for _ in range(n+1)]
+    #작년순위
+    for i in range(n):
+        temp[rank[i]] = rank[i+1:]
+        indegree[rank[i]] = i
+    
+    #순위역전
+    for _ in range(int(input())):
+        a, b = map(int, input().split())
+        #b는 a로 갈 수 있다. -> b는 a로 갈 수 없다.(remove)
+        #a는 b로 갈 수 없다. -> a는 b로 갈 수 있다.(append)
+        if a in temp[b]:
+            temp[b].remove(a)
+            temp[a].append(b)
+            indegree[a] -= 1
+            indegree[b] += 1
+        #b는 a로 갈 수 없다. -> b는 a로 갈 수 있다.(append)
+        #a는 b로 갈 수 있다. -> a는 b로 갈 수 없다.(remove)
+        else:
+            temp[b].append(a)
+            temp[a].remove[b]
+            indegree[b] -= 1
+            indegree[a] += 1
+    
+    # 시작노드
+    q = []
+    for i in range(1, n+1):
+        #indegree[i]가 0이면
+        if not indegree[i]:
+            q.append(i)
+    if not q:
+        #시작노드 부재, 사이클
+        print('IMPOSSIBLE')
         continue
+    
+    #위상정렬
+    ans = []
+    while q:
+        v = q.pop(0)
+        ans.append(v)
+        
 
-    result = True
-    while queue:
-        if len(queue) > 1:
-            result = False
-            break
 
-        tmp = queue.popleft()
-        answer.append(tmp)
-        for j in graph[tmp]:
-            inDegree[j] -= 1
-            if inDegree[j] == 0:
-                queue.append(j)
-            elif inDegree[j] < 0:
-                result = False
-                break
 
-    if not result or len(answer) < n:
-        print("IMPOSSIBLE")
-    else:
-        print(*answer)
+
+
