0c8adb1724a7ee18fd934da8000c3f2e0917cfcb
gygy7151 <gygy7151@gmail.com>
2022-04-08 01:58:06 +0900

    feat: solved 15684 of BOJ

--- BOJ/greedy/15684.py
+++ BOJ/greedy/15684.py
@@ -1,48 +1,47 @@
 '''
 사다리조작 - 15684 dfs로 접근
 '''
+N, M, H = map(int, input().split())
+board = [[0] * (N+1) for _ in range(H+1)]
+candidate = []
+ans = 4
+for _ in range(M):
+    a, b = map(int, input().split())
+    board[a][b] = 1
 
-from multiprocessing.connection import answer_challenge
+if M == 0:
+    print(0)
 
+for i in range(1, H+1):
+    for j in range(1, N):
+        if board[i][j] == False and board[i][j-1] == False and board[i][j+1] == False:
+            candidate.append([i,j])
+
+def dfs(cnt, num):
+    global ans
+    if cnt >= ans:
+        return
+    if check():
+        ans = cnt
+        return
+    for idx in range(num + 1, len(candidate)):
+        i, j = candidate[idx]
+        if board[i][j-1] == False and board[i][j+1] == False:
+            board[i][j] = True
+            dfs(cnt + 1, idx)
+            board[i][j] = False
 
 def check():
-    for start in range(N):
-        k = start
-        for j in range(H):
-            if board[j][k]:
-                k += 1 
-            elif k > 0  and board[j][k-1]:
-                k -= 1
-        if k != start: return False
+    for i in range(1, N+1):
+        now = i
+        for j in range(1, H+1):
+            if board[j][now] == True:
+                now += 1
+            elif board[j][now-1] == True:
+                now -= 1
+        if i != now:
+            return False
     return True
 
-def dfs(cnt, x, y):
-    global ans
-    if check():
-        ans = min(ans, cnt)
-        return
-    elif cnt == 3 or ans <= cnt:
-        return
-    
-    for i in range(x, H):
-        if i == x: k = y
-        else: k = 0
-        for j in range(k, N - 1):
-            if not board[i][j] and not board[i][j + 1]:
-                if j > 0 and board[i][j -1]: continue
-                board[i][j] = True
-                dfs(cnt + 1, i, j + 2)
-                board[i][j] = False
-
-if __name__ == '__main__':
-    N, M, H = map(int, input().split())
-    board = [[False] * N  for _ in range(H)]
-    if M == 0:
-        print(0)
-        exit(0)
-    for _ in range(M):
-        a, b = map(int, input().split())
-        board[a-1][b-1] = True
-    ans = 4
-    dfs(0, 0, 0)
-    print(ans if ans < 4 else -1)
+dfs(0, -1)
+print(ans if ans < 4 else -1)
