6cfdc827e922c0e984f29aea835d7c354d9ecd4e
gygy7151 <gygy7151@gmail.com>
2022-06-14 03:45:47 +0900

    feat: solved 2675 of BOJ

--- BOJ/simulation/2675.py
+++ BOJ/simulation/2675.py
@@ -1,3 +1,20 @@
 '''
 문자열 반복
 '''
+for i in range(int(input())):
+    R, S = map(str, input().split())
+    res = ''
+    S = list(S)
+
+    for s in S:
+
+        for j in range(int(R)):
+
+            if s == '\\':
+                res += '\\'+ s
+
+            else:
+                res += s
+    
+    print(res)
+
