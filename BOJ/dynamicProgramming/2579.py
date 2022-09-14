1c0302811595bf025b61cc46aa8d6d6b11fbe1fa
gygy7151 <gygy7151@gmail.com>
2022-09-14 22:11:03 +0900

    feat: solved 2579 of BOJ in dp

--- BOJ/dynamicProgramming/2579.py
+++ BOJ/dynamicProgramming/2579.py
@@ -2,36 +2,65 @@
 계단오르기
 '''
 '''
-두번째/세번째풀이 - DP, 3미만의 수 예외처리 - 인덱스에러해결
+세번째풀이 - 우리가 구해야할꺼는? 밟지 않았을때의 경우의 수니까
+        모든계단을 다 밟은 점수에서 밟지않았을때의 최소한의 점수만 빼주면 됨
 '''
-import sys
-input = sys.stdin.readline
-
 def solution():
     N = int(input())
-    STAIRS = []
-    
+
+    stairs = [0]
+    total = 0
     for _ in range(N):
-        #입력값에 \n없애주고 int형으로 변환해주는거 잊지말자..
-        STAIRS.append(int(input().rstrip()))
+        num = int(input())
+        stairs.append(num)
+        total += num
+    D = [0 for _ in range(N+1)]
     
-    DP = []
+    # 2보다 작은경우는 모든 계단 다 밟을 수 있는거니깐 점수 걍 다 합한거 출력하면 그만임
+    if N <=2:
+        print(total)
+        return
 
-    #계단의 갯수가 3개미만인경우
-    if N == 1:
-        return STAIRS[0]
-    if N == 2:
-        return max(STAIRS[1], STAIRS[0] + STAIRS[1])
+    D[1], D[2], D[3] = stairs[1], stairs[2], stairs[3]
+
+    for i in range(4, N):
+        D[i] = min(D[i-3], D[i-2]) + stairs[i]
+
+    print(total - min(D[N-1], D[N-2])) # N번째 계단은 당연히 안밟을꺼니깐
+solution()
+
+
+'''
+두번째/세번째풀이 - DP, 3미만의 수 예외처리 - 인덱스에러해결
+'''
+# import sys
+# input = sys.stdin.readline
+
+# def solution():
+#     N = int(input())
+#     STAIRS = []
+    
+#     for _ in range(N):
+#         #입력값에 \n없애주고 int형으로 변환해주는거 잊지말자..
+#         STAIRS.append(int(input().rstrip()))
+    
+#     DP = []
+
+#     #계단의 갯수가 3개미만인경우
+#     if N == 1:
+#         return STAIRS[0]
+#     if N == 2:
+#         return max(STAIRS[1], STAIRS[0] + STAIRS[1])
         
-    DP.append(STAIRS[0])
-    DP.append(max(STAIRS[1], STAIRS[0] + STAIRS[1]))
-    DP.append(max(STAIRS[0] + STAIRS[2], STAIRS[1] + STAIRS[2]))
+#     DP.append(STAIRS[0])
+#     DP.append(max(STAIRS[1], STAIRS[0] + STAIRS[1]))
+#     DP.append(max(STAIRS[0] + STAIRS[2], STAIRS[1] + STAIRS[2]))
     
-    for i in range(3,N):
-        DP.append( max(DP[i-2] + STAIRS[i], DP[i-3] + STAIRS[i] + STAIRS[i-1]) )
+#     for i in range(3,N):
+#         DP.append( max(DP[i-2] + STAIRS[i], DP[i-3] + STAIRS[i] + STAIRS[i-1]) )
 
-    return DP[N-1]
-print(solution())
+#     return DP[N-1]
+# print(solution())
 
 '''
 첫번째풀이 - 틀림
