704cabb7763e03ceba38094d1ee4c7b9d4a780a2
gygy7151 <gygy7151@gmail.com>
2022-11-01 01:19:03 +0900

    feat: solved 2193 of BOJ

---
+++ BOJ/implementation/2193.py
@@ -0,0 +1,21 @@
+'''
+이친수
+'''
+'''
+첫번/두번째풀이
+'''
+def solution():
+    N = int(input())
+    #예외처리 DP 만들기 전에 해줘야됨 주의
+    if N < 3:
+        print(1)
+        return
+    DP = [0 for _ in range(N+1)]
+    DP[1] = 1
+    DP[2] = 1
+
+    for idx in range(3, N+1):
+        DP[idx] = DP[idx-1] + DP[idx-2]
+    
+    print(DP[N])
+solution()
