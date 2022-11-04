125e19870d2040a3154e5a172a030945f84130f9
gygy7151 <gygy7151@gmail.com>
2022-11-04 18:02:26 +0900

    feat: solved 9251 of BOJ in dp

--- BOJ/implementation/2669.py
+++ BOJ/implementation/2669.py
@@ -8,6 +8,7 @@ import sys
 def solution():
     graph = [[0 for _ in range(101)] for _ in range(101)]
     
+    #네개만 받는다는걸 놓쳤다..
     for _ in range(4):
         x1, y1, x2, y2 = map(int, input().split())
 
